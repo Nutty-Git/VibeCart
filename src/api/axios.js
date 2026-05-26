b907c03645e1403a1ee007dca4685914ea363981
Natasha Romanoff <natsromanoff31@gmail.com>
2026-05-26 21:59:33 +0530

    Change axios base URL to localhost for development

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://vibecart-backend-o516.onrender.com/api",
+  baseURL: "http://localhost:5050/api",
 });
 
 export default instance;
